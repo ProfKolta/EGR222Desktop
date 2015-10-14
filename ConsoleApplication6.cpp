bdcb2b205561894b21004f1b589c25809efb90b8
Michael Kolta <mkolta@CBU3051.CalBaptist.local>
2015-10-14 07:24:44 -0700

    first file
    cpp

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include <iostream>
+#include <cmath>
+#include <ctime>
+
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	srand(time(0));
+	//cout << rand() % 3 + 1;
+	cout << rand() % 3 + 1;
+
+	return 0;
+}
